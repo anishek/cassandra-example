31a1ce402806f5c31516441e378adcbfd3e3f60f
Anishek Agarwal <anishek@adnear.com>
2015-03-20 12:18:03 +0530

    doing custom ordering

--- src/test/java/com/anishek/ReadSpecificNumberOfRecords.java
+++ src/test/java/com/anishek/ReadSpecificNumberOfRecords.java
@@ -3,8 +3,6 @@ package com.anishek;
 import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Session;
-import com.datastax.driver.core.Statement;
-import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.google.common.base.Stopwatch;
 
 import java.util.Map;
@@ -34,7 +32,7 @@ public class ReadSpecificNumberOfRecords implements Callable<ReadResult> {
     @Override
     public ReadResult call() throws Exception {
         long timePerReadInMicro = 0;
-        PreparedStatement prepare = session.prepare("select * From test.t1 where id=? limit ?");
+        PreparedStatement prepare = session.prepare("select * From test.t1 where id=? order by ts desc limit ?");
         for (long i = start; i < stop; i++) {
             long partitionId = i % totalPartitionKeys;
 //            Statement statement = QueryBuilder.select()
