1a612f9c9e3c85ab3efacd4cb80730d11953684a
Anishek Agarwal <anishek@adnear.com>
2015-03-23 12:23:42 +0530

    reading random partition.

--- src/test/java/com/anishek/ReadSpecificNumberOfRecords.java
+++ src/test/java/com/anishek/ReadSpecificNumberOfRecords.java
@@ -6,6 +6,7 @@ import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.google.common.base.Stopwatch;
 
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +17,7 @@ public class ReadSpecificNumberOfRecords implements Callable<ReadResult> {
     private Session session;
     private final int recordsToRead;
     private final long totalPartitionKeys;
+    private Random random = new Random(System.nanoTime());
 
 
     public ReadSpecificNumberOfRecords(long start, long stop, Map<String, Object> otherArguments) {
@@ -31,7 +33,7 @@ public class ReadSpecificNumberOfRecords implements Callable<ReadResult> {
     public ReadResult call() throws Exception {
         long timePerReadInMicro = 0;
         for (long i = start; i < stop; i++) {
-            long partitionId = i % totalPartitionKeys;
+            long partitionId = (i + random.nextLong()) % totalPartitionKeys;
             Statement statement = QueryBuilder.select()
                     .all().from("test", "t1")
                     .where(QueryBuilder.eq("id", partitionId))
