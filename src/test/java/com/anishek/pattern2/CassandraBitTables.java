9cd4018d2e93c22eda699167d9a3bd68b4483ee8
Anishek Agarwal <anishek@adnear.com>
2015-04-23 12:32:16 +0530

    and

--- src/test/java/com/anishek/pattern2/CassandraBitTables.java
+++ src/test/java/com/anishek/pattern2/CassandraBitTables.java
@@ -53,7 +53,7 @@ public class CassandraBitTables {
         session.close();
         session = cluster.connect("test");
         assertTrue(session.execute(" CREATE TABLE segments(id bigint , ts timestamp, segment_bits text, primary key (id, ts)) with clustering order by (ts desc) " +
-                "with gc_grace_seconds=0 " +
+                "and gc_grace_seconds=0 " +
                 "and compaction = {'class': 'DateTieredCompactionStrategy', 'timestamp_resolution':'SECONDS', 'base_time_seconds':'20', 'max_sstable_age_days':'0.5'} " +
                 "and compression={'sstable_compression' : ''};").wasApplied());
         session.close();
